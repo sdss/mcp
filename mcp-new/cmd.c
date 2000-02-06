5cdef9173ffd610af440248734bb8e5e229e5d82
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2000-02-06 05:58:40 +0000

    moved declaration of axis_cmds[] from .h to .c file

--- mcp-new/cmd.c
+++ mcp-new/cmd.c
@@ -51,9 +51,6 @@
 	5.  Echoed command is in upper case
 	6.  Data and Error output starts on a newline from the echoed command.
 --*/
-/*++ Notes:
-	Utilizes cmd.h which specifies the commands.
---*/
 /************************************************************************/
 
 /*------------------------------*/
@@ -74,6 +71,71 @@
 #include "pcdsp.h"
 #include "axis.h"
 #include "cmd.h"
+
+struct COMMANDS axis_cmds[] = {
+	{"\033",reboot_cmd},
+	{"CORRECT",correct_cmd},
+	{"DRIFT",drift_cmd},
+	{"ID",id_cmd},
+	{"INIT",init_cmd},
+	{"MAXACC",maxacc_cmd},
+	{"MAXVEL",maxvel_cmd},
+	{"MC.DUMP",mc_dump_cmd},
+	{"MC.MAXACC",mc_maxacc_cmd},
+	{"MC.MAXPOS",mc_maxpos_cmd},
+	{"MC.MAXVEL",mc_maxvel_cmd},
+	{"MC.MINPOS",mc_minpos_cmd},
+	{"MOVE",move_cmd},
+	{"+MOVE",plus_move_cmd},
+	{"MR.DUMP",mr_dump_cmd},
+	{"MS.DUMP",ms_dump_cmd},
+	{"MS.MAP.DUMP",ms_map_dump_cmd},
+	{"MS.MAP.LOAD",ms_map_load_cmd},
+	{"MS.OFF",ms_off_cmd},
+	{"MS.ON",ms_on_cmd},
+	{"MS.POS.DUMP",ms_pos_dump_cmd},
+	{"REMAP",remap_cmd},
+	{"IR",rot_cmd},
+	{"SET.LIMITS",set_limits_cmd},
+	{"SET.POSITION",set_position_cmd},
+	{"SET.TIME",set_time_cmd},
+	{"STATS",stats_cmd},
+	{"STATUS.LONG",status_long_cmd},
+	{"STATUS",status_cmd},
+	{"TEL1",tel1_cmd},
+	{"TEL2",tel2_cmd},
+	{"TICKLOST @ .",ticklost_cmd},
+	{"TIME?",time_cmd},
+	{"BRAKE.ON",brakeon_cmd},
+	{"BRAKE.OFF",brakeoff_cmd},
+	{"CLAMP.ON",clampon_cmd},
+	{"CLAMP.OFF",clampoff_cmd},
+	{"CWMOV",cwmov_cmd},
+	{"CWPOS",cwpos_cmd},
+	{"CWINST",cwinst_cmd},
+	{"CWABORT",cwabort_cmd},
+	{"CWSTATUS",cwstatus_cmd},
+	{"SP1",sp1_cmd},
+	{"SP2",sp2_cmd},
+	{"SLIT.CLEAR",slitclear_cmd},
+	{"SLIT.CLOSE",slitclose_cmd},
+	{"SLIT.OPEN",slitopen_cmd},
+	{"CART.LATCH",cartlatch_cmd},
+	{"CART.UNLATCH",cartunlatch_cmd},
+	{"SLIT.STATUS",slitstatus_cmd},
+	{"FFS.CLOSE",ffsclose_cmd},
+	{"FFS.OPEN",ffsopen_cmd},
+	{"FFL.ON",fflon_cmd},
+	{"FFL.OFF",ffloff_cmd},
+	{"NE.ON",neon_cmd},
+	{"NE.OFF",neoff_cmd},
+	{"HGCD.ON",hgcdon_cmd},
+	{"HGCD.OFF",hgcdoff_cmd},
+	{"FF.STATUS",ffstatus_cmd},
+	{"AB.STATUS",abstatus_cmd},
+	{"VERSION",version_cmd},
+	{NULL,dummy_cmd}		/* termination */
+};
 
 /*========================================================================
 **========================================================================
