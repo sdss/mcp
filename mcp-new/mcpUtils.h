d7584be82f088586c7ecd3304f38ee23bdd1dcd2
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2000-02-16 00:01:50 +0000

    added mjd() and open_log()

--- mcp-new/mcpUtils.h
+++ mcp-new/mcpUtils.h
@@ -3,6 +3,8 @@
 
 long date(void);
 char *get_date(void);
+int mjd(void);
+int open_log(const char *file);
 unsigned long timer_read(int timer);
 unsigned long timer_start(int timer);
 void VME2_pre_scaler(unsigned long adjust);
