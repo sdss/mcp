331e38405743308d2a1cea62f9676d88718bc13e
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2000-03-04 21:46:25 +0000

    added global client_pid

--- mcp-new/cmd.h
+++ mcp-new/cmd.h
@@ -6,14 +6,20 @@ int cmd_init();
 char *cmd_handler(char *cmd);
 void get_ctrl(SEM_ID sem), rtn_ctrl(SEM_ID sem);
 
+/*
+ * commands will match incorrectly if the longest command is not first
+ * with a subset of the string...i.e. CORRECTLY must come befor CORRECT
+ * since CORRECTLY matches both CORRECTLY and CORRECT
+ */
 struct COMMANDS {
 	char *command;
 	char *(*function)();
 };
-/* commands will match incorrectly if the longest command is not first */
-/* with a subset of the string...i.e. CORRECTLY must come befor CORRECT */
-/* since CORRECTLY matches both CORRECTLY and CORRECT */
+
 extern struct COMMANDS axis_cmds[];
+
+extern int client_pid;			/* Process ID of cmdPort telnet proc */
+
 
 #define __CMD_H__             /* do only once */
 
