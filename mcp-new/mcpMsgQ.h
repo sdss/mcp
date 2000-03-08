33211c67cef2016ca05afc34ce74b2e20b072a3b
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2000-03-08 20:16:04 +0000

    added counter weight messages

--- mcp-new/mcpMsgQ.h
+++ mcp-new/mcpMsgQ.h
@@ -3,16 +3,41 @@
 
 #include <msgQLib.h>
 
+#define USE_MSG_Q 1
+
 typedef struct {
    enum {
-      DIO316ClearISR_type
+      DIO316ClearISR_type,
+      moveCW_type,			/* counterweights motion */
+      moveCWAbort_type			/* abort counterweights */
    } type;
    union {
       struct {
 	 int timeout;
 	 unsigned char dio316int_bit;
       } DIO316ClearISR;
+
+      struct {
+	 int inst;			/* instrument to balance for */
+	 int cw;			/* CW to move, or ALL_CW */
+	 int cwpos;			/* desired position, or 0 to balance */
+      } moveCW;
+
+      struct {
+	 int abort;			/* always true */
+      } moveCWAbort;
    } u;
 } MCP_MSG;
+
+/*
+ * moveCW task
+ */
+extern MSG_Q_ID msgMoveCW;
+extern MSG_Q_ID msgMoveCWAbort;
+extern SEM_ID semMoveCWBusy;
+/*
+ * tm_ClrInt task
+ */
+extern MSG_Q_ID msgDIO316ClearISR;
 
 #endif
