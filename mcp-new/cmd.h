79625997d7646c9b4f3a7918c11a0c413032180e
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2000-03-12 00:00:11 +0000

    changed COMMANDS struct to encode information on number of args etc.

--- mcp-new/cmd.h
+++ mcp-new/cmd.h
@@ -2,19 +2,31 @@
 
 #ifndef __CMD_H__
 /* function prototypes */
-int cmd_init();
-char *cmd_handler(char *cmd);
-void get_ctrl(SEM_ID sem), rtn_ctrl(SEM_ID sem);
+int cmd_init(void);
+char *cmd_handler(int have_semPortCmd, char *cmd);
 
+#if !USE_VX_SYMBOL_TBL
 /*
  * commands will match incorrectly if the longest command is not first
  * with a subset of the string...i.e. CORRECTLY must come befor CORRECT
  * since CORRECTLY matches both CORRECTLY and CORRECT
  */
+#endif
 struct COMMANDS {
-	char *command;
-	char *(*function)();
+   char *command;			/* name of command */
+   char *(*function)(char *);		/* function to call */
+   int narg;				/* no. of arguments; -ve if variable */
+   int locked;				/* does command need the semaphore? */
+   int murmur;				/* should this command go to murmur? */
 };
+
+#define CMD_TYPE_NARG 0x7		/* mask in type for number of args */
+#define CMD_TYPE_VARARG 0x10		/* variable number of args */
+#define CMD_TYPE_LOCKED 0x20		/* cmd is locked; needs semCmdPort */
+#define CMD_TYPE_MURMUR 0x40		/* send to murmur by default */
+
+void define_cmd(char *name, char *(*addr)(char *),
+		int narg, int locked, int murmur);
 
 extern struct COMMANDS axis_cmds[];
 
