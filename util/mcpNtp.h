d1b69e0ba8bb1203f6bd4a88875598ba2028cfb2
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2000-03-26 21:46:00 +0000

    initial version; prototype for setTimeFromNTP()

---
+++ util/mcpNtp.h
@@ -0,0 +1,9 @@
+#if !defined(MCPNTP_H)
+#define MCPNTP_H
+/*
+ * The MCP's NTP command
+ */
+int setTimeFromNTP(const char *NTPserver_name, int retryDelay,		
+		   unsigned long retryCnt, int forceStep);
+
+#endif
