ef6a0192346544023456238580e4e6596d4fb337
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2001-01-08 03:17:11 +0000

    added getSemTaskId()

--- mcp-new/mcpUtils.h
+++ mcp-new/mcpUtils.h
@@ -2,6 +2,7 @@
 #define MCPUTILS_H
 
 #include <stdio.h>
+#include <semLib.h>
 
 char *get_date(void);
 int mjd(void);
@@ -11,5 +12,6 @@ unsigned long timer_start(int timer);
 void VME2_pre_scaler(unsigned long adjust);
 char *mcpVersion(char *ver, int len);
 char *version_cmd(char *cmd);
+long getSemTaskId(SEM_ID sem);
 
 #endif
