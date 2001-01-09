acf8f3ce5104deeb57639fc68b64362d9ab5d04a
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2001-01-09 18:15:56 +0000

    change attach_ihandler()'s handler type to "void (*)(int)"

--- ip/acromag/acromag.h
+++ ip/acromag/acromag.h
@@ -1,7 +1,7 @@
 #if !defined(ACROMAG_H)
 #define ACROMAG_H 1
 
-int attach_ihandler(int trap, int vector, int zero, int (*handler)(int),
+int attach_ihandler(int trap, int vector, int zero, void (*handler)(int),
 		    struct handler_data* hdata);
 UWORD inpw(UWORD *addr);
 UWORD outpw(UWORD *addr, int b);
