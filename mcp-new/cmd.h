59f0cb31d701bcea651092f38f782141ae67d243
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2001-01-09 03:32:40 +0000

    1/ added semCmdPortOwner[] 2/ added {take,give}_semCmdPort()

--- mcp-new/cmd.h
+++ mcp-new/cmd.h
@@ -3,6 +3,7 @@
 
 extern SEM_ID semCmdPort;		/* semaphore to control permission
 					   to axis motions etc. */
+extern char semCmdPortOwner[];		/* semCmdPort's owner */
 
 #define CMD_TYPE_NARG 0x7		/* mask in type for number of args */
 #define CMD_TYPE_VARARG 0x10		/* variable number of args */
@@ -15,5 +16,8 @@ void define_cmd(char *name, char *(*addr)(char *),
 		int narg, int locked, int murmur);
 
 extern int client_pid;			/* Process ID of cmdPort telnet proc */
+
+int take_semCmdPort(int timeout, char *id);
+int give_semCmdPort(int force);
 
 #endif
