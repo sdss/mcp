54330202a641304f56ab9a9bff3d7efd670d4496
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2001-05-29 20:06:33 +0000

    use dscTrace.h not trace.h, so messages will go to murmur

--- mcp-new/ipcast.c
+++ mcp-new/ipcast.c
@@ -15,7 +15,7 @@
 #include "errnoLib.h"
 #include "sockLib.h"
 #include "socket.h"
-#include "trace.h"
+#include "dscTrace.h"
 
 /*------------------------------------------------------------------------
 **
