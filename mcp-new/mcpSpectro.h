d8feafdb5939a031af25ca7e7c48201e45e02c46
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2003-08-01 18:16:03 +0000

    Initial version

---
+++ mcp-new/mcpSpectro.h
@@ -0,0 +1,12 @@
+#if !defined(MCPSPECTRO_H)
+#define MCPSPECTRO_H
+
+char *sp1_cmd(char *cmd);
+char *sp2_cmd(char *cmd);
+
+char *slitdoor_open_cmd(char *cmd);
+char *slitdoor_close_cmd(char *cmd);
+char *slithead_latch_open_cmd(char *cmd);
+char *slithead_latch_close_cmd(char *cmd);
+
+#endif
