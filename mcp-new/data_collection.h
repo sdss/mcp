bd51a9775ce7db905ebe5d16d361cc624c8163ec
sjnk <sjnk@D-173-250-189-251.dhcp4.washington.edu>
2004-01-31 04:12:54 +0000

    now has PLC version 18 number properly

--- mcp-new/data_collection.h
+++ mcp-new/data_collection.h
@@ -177,7 +177,7 @@ typedef struct {
    unsigned cartridge_1 : 1;                  /* Cartridge 1 on the telescope */
    unsigned no_inst_in_place : 1;             /* No Instrument on the telescope */
    unsigned disc_cable : 1;                   /* Instrument ID Cable Disconnected */
-   unsigned version_id : 16;                  /* Version 17  $Name$ */
+   unsigned version_id : 16;                  /* Version 18  $Name$ */
 } B3_L1;
 
 typedef struct {
@@ -1398,7 +1398,7 @@ typedef struct {
  * Version from PLC
  */
 #if defined(DATA_COLLECTION_C)
-   static char plcVersion[] = "Version 17  $Name$";
+   static char plcVersion[] = "Version 18  $Name$";
 #endif
 /*
  * End of machine generated code
