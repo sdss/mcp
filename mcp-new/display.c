6568178279ec5b49cacd6f847e15ddef5ce12fbb
rhl <rhl@D-173-250-189-251.dhcp4.washington.edu>
2005-08-09 17:26:23 +0000

    Included display.h inline so that I can delete it from cvs

--- mcp-new/display.c
+++ mcp-new/display.c
@@ -3,7 +3,6 @@
 /**************************************************************************/
 #include <stdio.h>
 #include <ctype.h>
-#include "display.h"
 #include "tickLib.h"
 #include "usrLib.h"
 #include "time.h"
@@ -28,6 +27,50 @@
 #include "cw.h"
 #include "mcpFiducials.h"
 #include "cmd.h"
+
+/*****************************************************************************/
+/*
+Copyright (C) Fermi National Accelerator Laboratory
+Filename: display.h
+Revision: 1.1
+Date and Time: 04/04/96 13:46:48
+*/
+/******************************************************************************/
+/*
+ display.h
+ Display Screen Functions
+ Jeff Utterback
+ originated 3/3/94
+
+These functions can be used with a Vt220 terminal to position the cursor,
+erase the screen etc.
+
+*/
+/******************************************************************************/
+#ifndef Display_H
+#define Display_H
+/******************************************************************************/
+/* DEFINES */
+/******************************************************************************/
+/* TYPES */
+/******************************************************************************/
+/* GLOBAL VARS */
+extern char *inst_display;  
+extern int inst_answer;
+
+/******************************************************************************/
+/* FUNCTION PROTOTYPES */
+/******************************************************************************/
+void EraseDisplayAll();
+void EraseDisplayRest();
+void CursPos(int X,int Y);
+void SaveCursPos();
+void RestoreCursPos();
+char GetCharNoEcho();
+/******************************************************************************/
+#endif     
+
+/*****************************************************************************/
 
 /*------------------------------------------------------------------------
 **
